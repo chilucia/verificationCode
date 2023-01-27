0936601b5ea65369a2a7ee2c25180b4abb481f0b
unknown <you@example.com>
2023-01-27 15:39:42 +0100

    Work done

--- controllers/AddUser.js
+++ controllers/AddUser.js
@@ -97,3 +97,79 @@ exports.UserVerify = async (req, res) => {
 }
 
 
+exports.Forgotpassword = async (req, res) => {
+    try{
+        const {email} = req.body
+        const userEmail = await AddUser.findOne({email})
+        if(!userEmail) return  res.status(404).json({ message: "No Email" })
+        const myToken = jwt.sign({
+            id:userEmail._id,
+            IsAdmin:userEmail.isAdmin}, process.env.JWTTOKEN, {expiresIn: "1m"})
+
+        const VerifyLink = `${req.protocol}://${req.get("host")}/api/changepassword/${userEmail._id}/${myToken}`
+        const message = `Use this link ${VerifyLink} to change your password`;
+        sendEmail({
+          email: userEmail.email,
+          subject: "Reset Pasword",
+          message,
+        })
+        
+        res.status(202).json({
+            message:"email have been sent"
+        })
+
+        // console.log(userEmail);
+    }catch(err){
+        res.status(400).json({
+            message:err.message
+        })
+    }
+}
+
+exports.resetpassword = async (req, res) => {
+    try {
+        const {password} = req.body
+        const id = req.params.id
+        const passwordchange = await AddUser.findById(id)
+        const salt = bcryptjs.genSaltSync(10);
+        const hash = bcryptjs.hashSync(password, salt);
+
+        await AddUser.findByIdAndUpdate(passwordchange._id,{
+            password: hash
+        },{new: true})
+
+        res.status(202).json({
+            message:"password updated"
+        })
+
+    } catch (err) {
+        res.status(400).json({
+            message:err.message
+        })
+    }
+}
+
+// exports.changepassword = async (req, res) => {
+//     try {
+//         const {password} = req.body
+//         const id = req.params.id
+//         const passwordchange = await AddUser.findById(id)
+//         const isPassword =await bcryptjs.compare(password,passwordchange.password)
+//         const salt = bcryptjs.genSaltSync(10);
+//         const hash = bcryptjs.hashSync(password, salt);
+
+//         await AddUser.findByIdAndUpdate(passwordchange._id,{
+//             password: hash
+//         },{new: true})
+
+//         res.status(202).json({
+//             message:"password updated"
+//         })
+
+//     } catch (err) {
+//         res.status(400).json({
+//             message:err.message
+//         })
+//     }
+// }
+
--- router/user.js
+++ router/user.js
@@ -1,11 +1,14 @@
 const express = require('express')
 const {GetallBags} = require('../controllers/Bags')
-const {UserVerify} = require('../controllers/AddUser')
+const {UserVerify, Forgotpassword, resetpassword} = require('../controllers/AddUser')
 
 const Router = express.Router()
 
 Router.route('/user').get(GetallBags)
 Router.route('/userVerify/:userid').post(UserVerify)
+Router.route('/forgotpassword').post(Forgotpassword)
+Router.route('/changepassword/:id/:token').post(resetpassword)
+// Router.route('/changepassword/:id').post(changepassword)
 
 
 module.exports = Router
