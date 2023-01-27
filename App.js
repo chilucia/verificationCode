52e9712a8712daf99f441699f6a7ca46af7960bf
unknown <you@example.com>
2023-01-27 16:11:31 +0100

    Adding cors

--- App.js
+++ App.js
@@ -1,11 +1,13 @@
 const express = require('express')
 const dotenv = require('dotenv')
 dotenv.config({path: './config/config.env'})
+const cors = require('cors')
 const Auth = require('./router/AddUser')
 const adminroute = require('./router/adminRouter')
 const user = require('./router/user')
 
 const app = express()
+app.use(cors())
 app.use(express.json())
 
 app.use('/api', Auth)
